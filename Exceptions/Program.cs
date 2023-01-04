c6494f2e9dc47eb605e397f6ede3166bcd933e5a
Rodrigo Spicker Reis <79051753+rodrigospicker1@users.noreply.github.com>
2023-01-03 22:53:49 -0300

    exceptions

---
+++ Exceptions/Program.cs
@@ -0,0 +1,22 @@
+﻿// var arr = new int[3];
+
+// Salvar("");
+
+// static void Salvar(string texto){
+//     if(string.IsNullOrEmpty(texto))
+//         throw new ArgumentNullException("O texto não pode ser nulo ou vazio");
+// }
+
+var arr = new int[3];
+
+try{
+    for(var index = 0; index < 10; index++){
+        Console.WriteLine(arr[index]);
+    }
+}catch(Exception ex){
+    Console.WriteLine(ex.InnerException);
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Ops, algo deu errado");
+}finally{
+    Console.WriteLine("Chegu ao fim");
+}
