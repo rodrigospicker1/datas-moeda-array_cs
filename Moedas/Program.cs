59eea92723a58cda240e1cb23808122b251e894b
Rodrigo Spicker Reis <79051753+rodrigospicker1@users.noreply.github.com>
2023-01-03 21:53:30 -0300

    project

---
+++ Moedas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Moedas{
+
+    public static class Program{
+
+        public static void Main(string[] args){
+
+            Console.Clear();
+
+            decimal valor = 10.24m;
+            Console.WriteLine(
+                valor.ToString(
+                    "C",
+                    CultureInfo.CreateSpecificCulture("pt-BR")
+                )
+            );
+
+            Console.WriteLine(Math.Round(valor));
+            Console.WriteLine(Math.Ceiling(valor));
+            Console.WriteLine(Math.Floor(valor));
+            
+        }
+
+    }
+
+}
